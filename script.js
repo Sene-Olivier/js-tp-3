06af39096a42c33600ad256f8f2466cad7543cca
Sene-Olivier <128190061+Sene-Olivier@users.noreply.github.com>
2023-05-02 19:38:58 +0200

    version 1.2

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
-// Créer un événement au scroll
-
-// Réduire la navbar quand on descend vers le vite, la remettre à sa taille initiale si on remonte tout en haut
-
-// Faire apparaitre l'image de la partie improvise
-
-// Faire apparaitre la popup quand on est en bas du site
-
-// Bonus : quand on clicke sur la popup elle disparait pour toujours
-
-let lastScroll = 0;
-const img1 = document.querySelector ("#imgImprovise");
+let playOnce = true;
 
 window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+        navbar.style.height = "45px";
+    } else {
+        navbar.style.height = "90px";
+    }
 
-if (window.scrollY < lastScroll) {
-    navbar.style.top = 0;
-} else {
-    navbar.style.height = "50px";
-}
-   
+let scrollValue = (window.scrollY + window.innerHeight) / document.body.offsetHeight;
+
+    if (scrollValue > 0.45) {
+        imgImprovise.style.opacity = 1;
+        imgImprovise.style.transform = "none";
+    }
+
+    if (scrollValue > 0.85 && playOnce) {
+        popup.style.opacity = 1;
+        popup.style.transform = "none";
+        playOnce = false;
+    }
+
+
 });
 
-window.addEventListener("scroll", () => {
-
-if (window.scrollY > "200px") {
-    img1.style.opacity = "1";
-    img1.style.transform.translateX = 0;
-}
-
+closeBtn.addEventListener("click", () => {
+    popup.style.opacity = 0;
+    popup.style.transform = "translateX(500px)";
 });
+
 
 
