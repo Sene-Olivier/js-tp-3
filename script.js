a6b65aeca988d1214c8bf4ad4a8249c25fdc3da8
Sene-Olivier <128190061+Sene-Olivier@users.noreply.github.com>
2023-05-02 18:45:29 +0200

    version 1.1

--- script.js
+++ script.js
@@ -7,3 +7,27 @@
 // Faire apparaitre la popup quand on est en bas du site
 
 // Bonus : quand on clicke sur la popup elle disparait pour toujours
+
+let lastScroll = 0;
+const img1 = document.querySelector ("#imgImprovise");
+
+window.addEventListener("scroll", () => {
+
+if (window.scrollY < lastScroll) {
+    navbar.style.top = 0;
+} else {
+    navbar.style.height = "50px";
+}
+   
+});
+
+window.addEventListener("scroll", () => {
+
+if (window.scrollY > "200px") {
+    img1.style.opacity = "1";
+    img1.style.transform.translateX = 0;
+}
+
+});
+
+
